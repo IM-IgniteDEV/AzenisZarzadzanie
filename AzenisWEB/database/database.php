705236171df8ab5232cb4b096c6f0fb5ce43cbd7
GruubYje <36342928+GruubYje@users.noreply.github.com>
2018-03-15 16:37:00 +0100

    Started adding Alert's

--- AzenisWEB/database/database.php
+++ AzenisWEB/database/database.php
@@ -12,6 +12,7 @@ if (mysqli_connect_errno())
     exit;   
 }
 else {
+	
 }
 
 
---
+++ AzenisWEB/script/script.js
@@ -0,0 +1,10 @@
+function giveBan() {
+	var checkNick = document.getElementsByTagName("azenis")[1].value;
+
+	alert ("Ban zostal przyznany dla konta: " + checkNick + ".");
+}
+function giveKick() {
+	var checkNick = document.getElementsByTagName("azenis")[1].value;
+
+	alert ("Gracz o nicku: " + checkNick + " został wyrzucony z serwera.");
+}
