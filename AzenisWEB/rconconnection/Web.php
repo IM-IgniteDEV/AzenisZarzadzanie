1b5e1d3d40dbcd345300aa25b58e7b0b34b9d038
jasuogra <31209154+jasuogra@users.noreply.github.com>
2018-03-15 20:18:03 +0100

    Added RCON connection

---
+++ AzenisWEB/rconconnection/Web.php
@@ -0,0 +1,41 @@
+<?php
+	define( 'MQ_SERVER_ADDR', '164.132.59.211' ); 
+	define( 'MQ_SERVER_PORT', 0000 );
+	define( 'MQ_SERVER_PASS', 'azenishcminecraft' );
+	define( 'MQ_TIMEOUT', 2 );
+	
+	require __DIR__ . '/ServerConnection.php';
+    
+    $nick=$_POST['gracz'];
+	
+	echo "<pre>";
+	
+	try
+	{
+		$Rcon = new MinecraftRcon;
+		
+		$Rcon->Connect( MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_SERVER_PASS, MQ_TIMEOUT );
+		
+		$Data = $Rcon->Command("kick nick reason"); 
+		
+		if( $Data === false )
+		{
+			throw new MinecraftRconException( "Nie mozna bylo wykonac komendy." );
+		}
+		else if( StrLen( $Data ) == 0 )
+		{
+			throw new MinecraftRconException( "Zdobyto wynik komendy niestety jest on pusty." );
+		}
+		
+		echo HTMLSpecialChars( $Data );
+	}
+	catch( MinecraftRconException $e )
+	{
+		echo $e->getMessage( );
+	}
+	
+	$Rcon->Disconnect( );
+
+$urlthx="azenis.pl";
+header("Location: " . $urlthx);
+?>
