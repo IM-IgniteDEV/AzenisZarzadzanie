0e8a9b313ca1bfc10c23c4dc765e3fe5bea4eb18
GruubYje <36342928+GruubYje@users.noreply.github.com>
2018-03-15 20:06:23 +0100

    Added 1 mini thing

--- AzenisWEB/script/script.js
+++ AzenisWEB/script/script.js
@@ -1,10 +1,14 @@
+function setInfo() {
+	var setNick = document.getElementById("tNick").value;
+	var sNick = document.getElementById("nick").innerHTML;
+	
+	document.getElementById("nick").innerHTML = setNick;
+}
 function giveBan() {
-	var checkNick = document.getElementsByTagName("azenis")[1].value;
-
+	var checkNick = document.getElementById("nick").innerHTML;
 	alert ("Ban zostal przyznany dla konta: " + checkNick + ".");
 }
 function giveKick() {
-	var checkNick = document.getElementsByTagName("azenis")[1].value;
-
+	var checkNick = document.getElementById("nick").innerHTML;
 	alert ("Gracz o nicku: " + checkNick + " został wyrzucony z serwera.");
 }
