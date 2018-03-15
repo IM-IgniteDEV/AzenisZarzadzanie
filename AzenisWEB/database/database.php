c6a703844d3105f88eb5a1f3ad6d1789a19e0ae1
jasuogra <31209154+jasuogra@users.noreply.github.com>
2018-03-15 16:03:54 +0100

    Database connection
    added connection to the database

---
+++ AzenisWEB/database/database.php
@@ -0,0 +1,20 @@
+<?php
+$server = 'vps509488.ovh.net';
+$user = 'root';
+$password = 'azenisserwerowniapl332199';
+$database_name = 'gildie';
+  
+$db = mysqli_connect($server, $user, $password, $database_name);
+ 
+if (mysqli_connect_errno()) 
+{
+    echo 'Nie mozna polaczyc z baza danych.';
+    exit;   
+}
+else {
+}
+
+
+//code there because i don't know what's will be there so i must think about that with gruuby, you know bro.
+
+?>
